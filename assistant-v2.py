c39c0fd004912af1b543d48ca25b4d2f2863f79a
prismarinenetwork <132186884+prismarinenetwork@users.noreply.github.com>
2025-08-17 12:47:31 -0400

    Update assistant-v2.py

--- assistant-v2.py
+++ assistant-v2.py
@@ -59,6 +59,10 @@ try:
     import whisper
 except:
     os.system('pip install -U openai-whisper')
+try:
+    import pyttsx3
+except:
+    os.system('pip install pyttsx3')
 import ollama
 from gtts import gTTS
 import pygame 
@@ -67,44 +71,11 @@ import keyboard
 import time
 import threading
 import random
+import pyttsx3
 
-def random_filename():
-        #Before you say anything I know there is a wayyyyyyy faster way but this was coded at 3:00 AM
-        global filename
-        filename=''
-        random_1=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_2=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_3=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_4=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_5=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_6=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_7=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_8=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_9=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_10=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_11=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_12=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_13=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_14=random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
-        random_num_1=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_2=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_3=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_4=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_5=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_6=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_7=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_8=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_9=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        random_num_0=random.choice(['1','2','3','4','5','6','7','8','9','0'])
-        filename=random_1+random_2+random_3+random_4+random_5+random_6+random_7+random_8+random_9+random_10+random_11+random_12+random_13+random_14+random_num_1+random_num_2+random_num_3+random_num_4+random_num_5+random_num_6+random_num_7+random_num_8+random_num_9+random_num_0
-        return filename
-usr_prof=os.environ['USERPROFILE']
-os.chdir(usr_prof+'/Downloads')
 
-def play_file(mp3File):
-    pygame.mixer.init()
-    pygame.mixer.music.load(filename)
-    pygame.mixer.music.play()
+
+
 os.system('cls')
 def record_mic():
     global filename
@@ -135,24 +106,14 @@ def record_mic():
     ai_output = ai_output['response']
     print('Ai response:'+str(ai_output))
     print('')
-    tts = gTTS(ai_output,slow=False)
-    filename=str(random_filename())+'.mp3'
-    tts.save(filename)
-    return ai_output
-def music_helper(mp3File):
-    while pygame.mixer.music.get_busy():
-        time.sleep(0.50)
-    pygame.mixer.music.stop()
-    
-
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 190) 
+    engine.say(ai_output)
+    engine.runAndWait()
 
 while True:
     if keyboard.is_pressed('`'):
         mp3File=record_mic()
-        if mp3File:
-            playing_file_thread=threading.Thread(target=play_file, args=(mp3File,)).start()
-            music_helper_thread=threading.Thread(target=music_helper, args=(mp3File,)).start()
-            
         while keyboard.is_pressed('`'):
             pass
 #['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large', 'large-v3-turbo', 'turbo']
